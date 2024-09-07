08aa8884758e39637afbf58eb673974c9548bc0c
VASKO\eqinox <eqinox@abv.bg>
2024-09-07 15:38:12 +0300

    add access for the admin panel to visualize preview for the images

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,10 +9,10 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "img-src": ["'self'", "data:", "blob:", "https://market-assets.strapi.io", "https://sweetsurprises-bucket.s3.eu-north-1.amazonaws.com"],
+          "img-src": ["'self'", "data:", "blob:", "https://market-assets.strapi.io", "https://mebeli-nikolov.s3.eu-north-1.amazonaws.com"], // Update to your new S3 bucket URL
           "script-src": ["'self'", "'unsafe-inline'", "blob:"], // Allow inline scripts and blob URIs
           "worker-src": ["'self'", "blob:"], // Allow worker sources from blob URIs
-          "connect-src": ["'self'", "blob:", "https://sweetsurprises-bucket.s3.eu-north-1.amazonaws.com"], // Allow connections to specified sources
+          "connect-src": ["'self'", "blob:", "https://mebeli-nikolov.s3.eu-north-1.amazonaws.com"], // Update to your new S3 bucket URL
           "default-src": ["'self'"], // Default fallback for other unspecified directives
           "frame-src": ["'self'"], // Allow iframes from self (adjust if you use iframes from other sources)
         },
